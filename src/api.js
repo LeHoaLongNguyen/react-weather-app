84ec168d3f1ac4e6e793edf92571aa87c8561976
LeHoaLongNguyen <137870237+LeHoaLongNguyen@users.noreply.github.com>
2023-07-12 22:59:38 +1000

    first commit

---
+++ src/api.js
@@ -0,0 +1,12 @@
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '7cb143763emsh6ff7b3ab2a6bc3ap1b7be0jsn95fcfa5b4fa3',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo/"
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY = "3dc27e6a8cd2de4b40db85b7492e22bc"
